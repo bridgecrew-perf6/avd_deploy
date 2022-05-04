8342b290508357ffa42f0bb66159341572c2e123
Francisco Palá <francisco.palab@gmail.com>
2022-05-04 14:14:54 +0200

    second commit

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import requests
 
-url = 'http://127.0.0.1:5000//api/v1/retrain'
+server = 'http://127.0.0.1:5000'
+url = '{s}/api/v1/retrain'.format(server)
 params = {'tv': 325.5, 'radio': 54, 'newspaper': 2393}
 r = requests.get(url, params=params)
 r.encoding = 'utf-8'
